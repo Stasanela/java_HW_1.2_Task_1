802587d11df2d222dc718b1a86de6d0b07b2e5f2
stasanela <stasanela@yandex.ru>
2021-08-10 17:29:05 +0300

    second commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int expense = 2_000_000_000;
+        int amount = 500_000_000;
+        int total = expense + amount;
+        System.out.println(total);
+
+    }
+}
